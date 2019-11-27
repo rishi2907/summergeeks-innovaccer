1215ab8e946ea365e848b5e367e0ae6300145512
rishi2907 <rishiagrawal29071998@gmail.com>
2019-11-28 00:50:30 +0530

    complete api enhancement

--- routes/visitor.js
+++ routes/visitor.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 const db = require('../config/database');
 const visitor = require('../models/visitor');
+const log = require('../models/log');
 
 // to get all current checkin users
 router.get('/checkin', function(req, res, next) {
@@ -29,6 +30,43 @@ router.post('/checkin', function (request, response) {
             response.status(400).send('Error in insert new record');
         }
     });
+});
+
+
+
+router.post('/checkout', function (request, response) {
+
+    var id = request.body.id;
+    var checkOutTim = request.body.checkOutTime;
+
+    visitor.findByPk(id)
+        .then((visit) => log.create({
+            name: visit.name,
+            phone: visit.phone,
+            email: visit.email,
+            hostName:visit.hostName,
+            checkInTime : visit.checkInTime,
+            checkOutTime : checkOutTim,
+            Address: "Innovaccer Office"
+
+      }).then((visit)=> {
+            if (visit) {
+                visitor.destroy({
+                    where: {
+                        id: id //this will be your id that you want to delete
+                        }
+                        }).then(function(rowDeleted){ // rowDeleted will return number of rows deleted
+                          if(rowDeleted === 1){
+                            console.log('Deleted successfully');
+                          }
+                        }, function(err){
+                          console.log(err);
+                        });
+                response.send(visit);
+            } else {
+                response.status(400).send('Error in insert new record');
+            }
+        }))
 });
 
 module.exports = router;
