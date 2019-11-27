ee15ce54cdaf2973aadad051557385b4d41879f6
rishi2907 <rishiagrawal29071998@gmail.com>
2019-11-27 14:44:52 +0530

    isbf

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 // database
-module.exports = new Sequelize('ohqMpdfFHo', 'ohqMpdfFHo','LaxL0QBuj0',{
+module.exports = new Sequelize('tfGJn2sP7l', 'ohqMpdfFHo','LaxL0QBuj0',{
   host : 'remotemysql.com',
   dialect:'mysql',
   operatorsAliases:false,
