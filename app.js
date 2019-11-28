5d37859e96b1c2aa3342a3796fd89cc4fc01e868
Rishi Agrawal <33354478+rishi2907@users.noreply.github.com>
2019-11-28 18:13:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -58,11 +58,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   next();
+// });
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
 app.use('/host', hostRouter);
 app.use('/users', usersRouter);
 app.use('/visitor',visitorRouter);
