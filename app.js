4d18747189b2e4c2551a7ad5c479fead7e2d18f0
Rishi Agrawal <33354478+rishi2907@users.noreply.github.com>
2019-11-28 18:01:34 +0530

    corsbug

--- app.js
+++ app.js
@@ -26,21 +26,38 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-}
+// var allowCrossDomain = function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', 'example.com');
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+//     next();
+// }
+
+// //...
+// app.configure(function() {
+//     app.use(express.bodyParser());
+//     app.use(express.cookieParser());
+//     app.use(express.session({ secret: 'cool beans' }));
+//     app.use(express.methodOverride());
+//     app.use(allowCrossDomain);
+//     app.use(app.router);
+//     app.use(express.static(__dirname + '/public'));
+// });
+
+// var allowCrossDomain = function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', "*");
+//     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type');
+//     next();
+// }
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({
-  origin :'http://localhost:3000'
-}));
+app.use(cors());
 
 app.use('/host', hostRouter);
 app.use('/users', usersRouter);
