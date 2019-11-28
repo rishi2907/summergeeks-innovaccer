4c54c26cd39cbe12f2530d6c67fec79a9747f612
Rishi Agrawal <33354478+rishi2907@users.noreply.github.com>
2019-11-28 17:55:17 +0530

    update

--- app.js
+++ app.js
@@ -26,6 +26,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
